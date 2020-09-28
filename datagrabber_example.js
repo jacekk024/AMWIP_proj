cf6c320b585e3af6a774a3c074cadbf13bc98934
jacekk024 <45696277+jacekk024@users.noreply.github.com>
2020-09-28 20:40:00 +0200

    Add files via upload

--- datagrabber_example.js
+++ datagrabber_example.js
@@ -21,7 +21,10 @@ var chartContext;  ///< chart context i.e. object that "owns" chart
 var chart; ///< Chart.js object
 var timer; ///< request timer
 
-
+const columnsNumber = 8;
+const rowNumber = 8;
+var btnHolder = [];
+var currentColor = [0,0,0]
 
 
 var xdata_rpy;
@@ -44,10 +47,10 @@ var timer3;
 
 var timer4;/// timer to table
 
-
-const url = 'http://192.168.1.3/chartdata.json'; ///< server app with JSON API
-const vurl = 'http://150.254.137.66/WebApp/scripts/config.php';
-const curl ='WebApp/scripts/config.json';
+const qurl = 'http://192.168.1.3/ServerScripts/led_display.php'
+const url = 'http://192.168.1.3/ServerScripts/chartdata.json'; ///< server app with JSON API
+const vurl = 'http://150.254.137.66/config.php';
+const curl ='config.json';
 //function to configure settings
 function getConfig(){
 	/// Send request
@@ -68,7 +71,7 @@ function getConfig(){
 		},
 		/// error callback
 		error: function(response){
-		alert(response)}
+		}
 	});
 }
 
@@ -634,11 +637,9 @@ url: vurl,
 data: {data: jsonStr},
 type: 'POST',
 success: function(response) {
-  alert(response);
   console.log('success');
 },
 error: function(response) {
-  alert(response);
   console.log('fail');
 }
 
@@ -649,3 +650,84 @@ error: function(response) {
 window.onload = function (){
 $('#submitbtn').click(subFunction)
 }
+
+
+///function to generate buttons
+
+function generateBtns(){
+    for(let i = 0; i < rowNumber; i++){
+        for(let j = 0; j < columnsNumber; j++){
+            var btn = document.createElement("button");
+            var btnId = [i,j,0,0,0]
+            btn.setAttribute("id", btnId);
+            btn.onclick = function (){
+                changeColor(this.id);
+                sendToServer4(this.id);
+            }
+            document.getElementById('button-holder').appendChild(btn);
+            btnHolder.push(btnId);
+        }
+    }
+}
+function changeColor(str){
+    clickedBtn = document.getElementById(str)
+    var newId = (str.substr(0,4) + currentColor[0] + ',' + currentColor[1] + ',' + currentColor[2]);
+    clickedBtn.setAttribute("id", newId);
+    clickedBtn.style.backgroundColor  = ('rgb(' + currentColor[0] + ',' + currentColor[1] + ',' + currentColor[2] + ')');
+
+    for(let i = 0; i<columnsNumber*rowNumber; i++){
+        if(btnHolder[i][0] == newId[0] && btnHolder[i][1] == newId[2]){
+            btnHolder[i][2] = currentColor[0];
+            btnHolder[i][3] = currentColor[1];
+            btnHolder[i][4] = currentColor[2];
+        }
+    }
+}
+function sendToServer4(str){
+    console.log(str)
+    console.log(btnHolder)
+    var jsonObj = btnHolder
+    console.log(jsonObj);
+    var jsonStr = (JSON.stringify(jsonObj));
+    console.log(jsonStr);
+    $.ajax({
+        url: qurl,
+        data: {data: jsonStr},
+        type: 'POST',
+        success: function(response) {
+	           console.log('success');
+           },
+           error: function(response) {
+               console.log('fail');
+           }
+
+       });
+
+}
+function pickColor(){
+    var hex = document.getElementById("color-picker").value
+    console.log(hex);
+    var red = parseInt(hex.substr(1,2) , 16)
+    var green = parseInt(hex.substr(3,2) , 16)
+    var blue = parseInt(hex.substr(5,2) , 16)
+    console.log(red,green,blue)
+    currentColor[0] = red;
+    currentColor[1] = green;
+    currentColor[2] = blue;
+    console.log(currentColor)
+}
+function resetLeds(){
+    buttons = document.getElementById("button-holder");
+    console.log(buttons.childNodes[1])
+    for(let i = 0; i<columnsNumber*rowNumber; i++){
+        buttons.childNodes[i+1].style.background = "rgb(255,255,255)"
+        btnHolder[i][2] = 0;
+        btnHolder[i][3] = 0;
+        btnHolder[i][4] = 0;
+    }
+    sendToServer4()
+}
+$(document).ready(function (){
+    generateBtns();
+    $('#reset-button').click(resetLeds);
+});
